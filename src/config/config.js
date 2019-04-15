8a2bdcda2c357047ab201718399e52d6e827c300
deescobarc <deescobarc@unal.edu.co>
2019-04-14 23:26:28 -0500

    Se hace modificación al ver cursos no creados

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,9 @@
 process.env.PORT = process.env.PORT || 3000;
 
-process.env.URLDB = 'mongodb://localhost:27017/asignaturas'
+//process.env.URLDB = 'mongodb://localhost:27017/asignaturas'
+
+if(!process.env.URLDB){
+
+    process.env.URLDB = 'mongodb://localhost:27017/asignaturas'
+    
+}
