a56925887891aa92b5c1aed08def811ba066e068
deescobarc <deescobarc@unal.edu.co>
2019-04-29 22:34:11 -0500

    actualización

--- src/config/config.js
+++ src/config/config.js
@@ -7,4 +7,3 @@ if(!process.env.URLDB){
     process.env.URLDB = 'mongodb://localhost:27017/asignaturas'
     
 }
-process.env.SENDGRID_API_KEY = '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'
